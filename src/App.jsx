9422d2c1a1b5eaeb8cdaeb48c4a4a8d8af594208
Sushant Sharma <ssushantlb08@gmail.com>
2023-02-19 00:06:09 +0530

    ep3 assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { User } from './User'
+import { Planet } from './Planet'
 
 const App = () => {
   const users = [
@@ -8,10 +9,24 @@ const App = () => {
     { name: 'sparsh', age:20 },
     { name: 'pathak', age:20 },
   ]
+
+  const planets = [
+    {name:'Mars',isGasPlanet:false},
+    {name:'Earth',isGasPlanet:false},
+    {name:'Jupiter',isGasPlanet:true},
+    {name:'Venus',isGasPlanet:false},
+    {name:'Neptune',isGasPlanet:true},
+    {name:'Uranus',isGasPlanet:true}
+  ]
+
   return (
     <div className='App'>
       {users.map((user, key) => {
         return <User name={user.name} age={user.age}/>
+      })}
+      printing Gas planets only
+      {planets.map((planet,key)=>{
+       return <Planet name={planet.name} isGasPlanet={planet.isGasPlanet} />
       })}
     </div>
   )
---
+++ src/Planet.jsx
@@ -0,0 +1,8 @@
+ export const Planet=(props)=>{
+    return (
+        <div style={{display: props.isGasPlanet?'block':'none'}}>
+            <h1 >{props.name}</h1>
+        </div>
+    )
+ }
+ 
