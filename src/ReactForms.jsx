873af3c3961ffb434d47ca8ec112180ffddf8f1c
Sushant Sharma <ssushantlb08@gmail.com>
2023-04-08 23:48:42 +0530

    Update ReactForms.jsx

--- src/ReactForms.jsx
+++ src/ReactForms.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const ReactForms = () => {
   return (
-    <div className='form-main' style={{display:"flex", justifyContent:"center"}}>
-        <form style={{textAlign:"right"}}> 
+    <div className='form-main' style={{display:"flex",alignItems:"center", justifyContent:"center",flexDirection:"column"}}>
+        <h1>React Forms</h1>
+        <p style={{color:"gray"}}>This form has an submit feature which returns an object of data entered by the user</p>
+        <form style={{textAlign:"right",fontSize:"20px"}}> 
             <label htmlFor="fullname">Fullname : </label>
             <input type="text" id='fullname' name='fullname' placeholder='FullName' />
             <br />
